826ca1ab155c3dec53025be575e73bf065de7d39
zlidzen <m.karsakou@gmail.com>
2024-06-24 20:48:31 +0200

    add onClickScan action

--- app/src/main/java/com/example/mymagicmanager/MainActivity.kt
+++ app/src/main/java/com/example/mymagicmanager/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.mymagicmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.mymagicmanager.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingClass: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
+
+    fun onClickScan (view: View){
+        val intent = Intent(this, ScanActivity::class.java)
+        startActivity(intent)
+    }
+
 }
